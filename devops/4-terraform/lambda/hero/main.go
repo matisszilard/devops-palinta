6e41e79337199322ca77652528f53c41143eae43
Szilárd Mátis <matis.szilard@codespring.ro>
2020-08-25 17:58:50 +0300

    Updated aws terraform

--- devops/4-terraform/lambda/hero/main.go
+++ devops/4-terraform/lambda/hero/main.go
@@ -20,11 +20,19 @@ func HandleRequest(req events.APIGatewayProxyRequest) (events.APIGatewayProxyRes
 	var oath string
 	switch name {
 	case "Aragorn":
-		oath = "Take my sword"
+		oath = "If by my life or death I can protect you I will You have my sword."
 	case "Legolas":
-		oath = "And my bow"
+		oath = "And you have my bow"
 	case "Gimli":
 		oath = "And my axe"
+	case "Boromir":
+		oath = "You carry the fates of us all little one If this is indeed the will of the Council then Gondor will see it done."
+	case "Gandalf":
+		oath = "I will help you bear this burden Frodo Baggins as long as it is yours to bear."
+	case "Thanos":
+		oath = "Oops! Wrong universe!"
+	default:
+		oath = "I don't know who you are. I don't know what you want. If you are looking for the CI/CD. I can tell you I don't have it, but what I do have are a very particular set of skills. Skills I have acquired over a very long career (1 month). Skills that make me a nightmare for people like you. If you let the devops rule now that'll be the end of it. I will not look for you, I will not pursue you, but if you don't, I will look for you, I will find you and I will devopsify you."
 	}
 
 	res := events.APIGatewayProxyResponse{
